daed12317e35aba6f8171bf84f301734ad793282
Cielo <A-CIELOER@legal.regn.net>
2018-08-21 22:47:18 +0800

    Add project files.

---
+++ ClockWork.Model/Migrations/20180821101357_Initial.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClockWork.Model.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CurrentTimeQueries",
+                columns: table => new
+                {
+                    CurrentTimeQueryId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Time = table.Column<DateTime>(nullable: false),
+                    ClientIp = table.Column<string>(nullable: true),
+                    UTCTime = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrentTimeQueries", x => x.CurrentTimeQueryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimeZoneRequests",
+                columns: table => new
+                {
+                    RequestId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    RequestedTimezone = table.Column<string>(nullable: true),
+                    RequestDate = table.Column<DateTime>(nullable: false),
+                    ClientIp = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeZoneRequests", x => x.RequestId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurrentTimeQueries");
+
+            migrationBuilder.DropTable(
+                name: "TimeZoneRequests");
+        }
+    }
+}
---
+++ ClockWork.Web/Pages/Conversion.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClockWork.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClockWork.Web.Pages
+{
+    public class ConversionModel : PageModel
+    {
+        #region VIEWMODELS
+        public IEnumerable<CurrentTimeQuery> TimeQueries { get; set; }
+        #endregion
+        private readonly ClockWorkContext _dbContext;
+        public ConversionModel(ClockWorkContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void OnGet()
+        {
+            TimeQueries = _dbContext.CurrentTimeQueries.ToList();
+        }
+    }
+}
